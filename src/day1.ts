a221037a98c274fd2a2e25262ef9c167ad1147a6
Tim Peters <tim@catminion.net>
2023-12-02 15:37:36 +1030

    Day 1 part 2

--- src/day1.ts
+++ src/day1.ts
@@ -10,3 +10,35 @@ function calibrationValue (str: string): number {
 const part1 = input.map(line => calibrationValue(line)).reduce((acc, v) => acc + v)
 
 console.log('Day 01, Part 1', part1)
+
+const words: Record<string, number> = {
+  one: 1,
+  two: 2,
+  three: 3,
+  four: 4,
+  five: 5,
+  six: 6,
+  seven: 7,
+  eight: 8,
+  nine: 9
+}
+
+function calibrationValue2 (str: string): number {
+  const digits: number[] = []
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] >= '0' && str[i] <= '9') {
+      digits.push(+str[i])
+    } else {
+      Object.keys(words).forEach(function (word) {
+        if (str.substring(i, i + word.length) === word) {
+          digits.push(words[word])
+        }
+      })
+    }
+  }
+  return (digits.at(0) ?? 0) * 10 + (digits.at(-1) ?? 0)
+}
+
+const part2 = input.map(line => calibrationValue2(line)).reduce((acc, v) => acc + v)
+
+console.log('Day 01, Part 2', part2)
