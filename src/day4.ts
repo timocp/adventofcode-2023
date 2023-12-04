d685bce6f975665421bf843a03165131fffc808b
Tim Peters <tim@catminion.net>
2023-12-04 21:28:39 +1030

    Day 4 part 2

--- src/day4.ts
+++ src/day4.ts
@@ -4,8 +4,7 @@ const input = readFileSync('input/day4.txt', 'utf8').trimEnd().split(/\n/)
 
 interface Card {
   num: number
-  winningNumbers: number[]
-  heldNumbers: number[]
+  hits: number
 }
 
 const cards: Card[] = input.map(function (line) {
@@ -15,13 +14,25 @@ const cards: Card[] = input.map(function (line) {
   const num = parseInt(m[1])
   const winningNumbers = m[2].split(/\s+/).map(s => parseInt(s))
   const heldNumbers = m[3].split(/\s+/).map(s => parseInt(s))
+  const hits = winningNumbers.filter(n => heldNumbers.includes(n)).length
 
-  return { num, winningNumbers, heldNumbers }
+  return { num, hits }
 })
 
 const part1 = cards.map(function (card) {
-  const hits = card.winningNumbers.filter(n => card.heldNumbers.includes(n)).length
-  return hits === 0 ? 0 : 2 ** (hits - 1)
+  return card.hits === 0 ? 0 : 2 ** (card.hits - 1)
 }).reduce((acc, v) => acc + v)
 
 console.log('Day 04, Part 1', part1)
+
+const cardCount = cards.map(_ => 1)
+
+cards.forEach(function (card, i) {
+  for (let c = 1; c <= card.hits; c++) {
+    cardCount[i + c] += cardCount[i]
+  }
+})
+
+const part2 = cardCount.reduce((acc, v) => acc + v)
+
+console.log('Day 04, Part 2', part2)
