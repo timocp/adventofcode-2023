ac8a0322b8a017316428886b7d081bb329ba8cdc
Tim Peters <tim@catminion.net>
2023-12-08 22:51:49 +1030

    Day 7 part 2

--- src/day7.ts
+++ src/day7.ts
@@ -34,35 +34,60 @@ class Hand {
 
   type: Type
 
-  constructor (cards: string, bid: number) {
+  constructor (cards: string, bid: number, jokerRule: boolean) {
     if (cards.length !== 5) throw new Error(`Expected 5 cards, got ${cards}`)
     this.cards = cards
     this.values = cards.split('').map(card => faceValue[card])
     this.bid = bid
-
-    const histogram = new Map<number, number>()
-    this.values.forEach(v => {
-      histogram.set(v, (histogram.get(v) ?? 0) + 1)
-    })
-    const entries = Array.from(histogram.entries())
-    if (histogram.size === 1) {
-      this.type = Type.fiveOfAKind
-    } else if (histogram.size === 2 && entries.some(e => e[1] === 4)) {
-      this.type = Type.fourOfAKind
-    } else if (histogram.size === 2 && entries.some(e => e[1] === 3)) {
-      this.type = Type.fullHouse
-    } else if (histogram.size === 3 && entries.some(e => e[1] === 3)) {
-      this.type = Type.threeOfAKind
-    } else if (histogram.size === 3 && entries.some(e => e[1] === 2)) {
-      this.type = Type.twoPair
-    } else if (histogram.size === 4 && entries.some(e => e[1] === 2)) {
-      this.type = Type.onePair
-    } else if (histogram.size === 5) {
-      this.type = Type.highCard
+    if (jokerRule) {
+      this.type = calcJokerType(this.values)
+      // transform jokers to a low value so they lose any face comparison
+      this.values = this.values.map(v => v === faceValue.J ? 1 : v)
     } else {
-      throw new Error(`Unmapped cards: ${cards}`)
+      this.type = calcType(this.values)
     }
   }
+}
+
+const calcType = (values: number[]): Type => {
+  const histogram = new Map<number, number>()
+  values.forEach(v => {
+    histogram.set(v, (histogram.get(v) ?? 0) + 1)
+  })
+  const entries = Array.from(histogram.entries())
+  if (histogram.size === 1) {
+    return Type.fiveOfAKind
+  } else if (histogram.size === 2 && entries.some(e => e[1] === 4)) {
+    return Type.fourOfAKind
+  } else if (histogram.size === 2 && entries.some(e => e[1] === 3)) {
+    return Type.fullHouse
+  } else if (histogram.size === 3 && entries.some(e => e[1] === 3)) {
+    return Type.threeOfAKind
+  } else if (histogram.size === 3 && entries.some(e => e[1] === 2)) {
+    return Type.twoPair
+  } else if (histogram.size === 4 && entries.some(e => e[1] === 2)) {
+    return Type.onePair
+  } else if (histogram.size === 5) {
+    return Type.highCard
+  } else {
+    throw new Error(`Unmapped cards: ${values.join(',')}`)
+  }
+}
+
+const calcJokerType = (values: number[]): Type => {
+  let type = calcType(values)
+  if (values.some(v => v === faceValue.J)) {
+    // Check if changing the joker value to any other value gives better hands
+    for (const replacement of Object.values(faceValue)) {
+      if (replacement !== faceValue.J) {
+        const newValues = values.map(v => v === faceValue.J ? replacement : v)
+        const newType = calcType(newValues)
+        if (newType < type) type = newType
+      }
+    }
+  }
+
+  return type
 }
 
 // return negative if a < b, positive if a > b, zero if a = b
@@ -77,21 +102,20 @@ const cmpCards = (a: Hand, b: Hand): number => {
 }
 
 export class Day7 extends Solution {
-  part1 (): number {
-    const hands = this.parseInput()
+  part1 = (): number => this.result(false)
+
+  part2 = (): number => this.result(true)
+
+  result (jokerRule: boolean): number {
+    const hands = this.parseInput(jokerRule)
     hands.sort(cmpCards)
-    // hands.forEach((h, rank) => { console.log(`${h.cards} (${h.type}) is rank ${rank + 1}`) })
     return sum(hands.map((h, rank) => h.bid * (rank + 1)))
   }
 
-  part2 (): number {
-    return 0
-  }
-
-  parseInput (): Hand[] {
+  parseInput (jokerRule: boolean): Hand[] {
     return this.inputLines().map(line => {
       const [cards, bid] = line.split(' ')
-      return new Hand(cards, +bid)
+      return new Hand(cards, +bid, jokerRule)
     })
   }
 }
