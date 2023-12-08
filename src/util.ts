5963f7f025ca34fd835cb2a9db63747d1761cc1a
Tim Peters <tim@catminion.net>
2023-12-09 08:10:51 +1030

    Add GCD, LCM functions

--- src/util.ts
+++ src/util.ts
@@ -3,3 +3,9 @@ export const sum: (a: number[]) => number = (a: number[]): number => a.reduce((a
 export const product: (a: number[]) => number = (a: number[]): number => a.reduce((acc, v) => acc * v)
 
 export const enumerate: (n: number) => number[] = (n: number): number[] => Array.from(Array(n).keys())
+
+export const gcd: (a: number, b: number) => number = (a: number, b: number): number => b === 0 ? a : gcd(b, a % b)
+
+export const lcm: (a: number, b: number) => number = (a: number, b: number): number => a / gcd(a, b) * b
+
+export const lcmAll: (a: number[]) => number = (a: number[]): number => a.reduce(lcm, 1)
