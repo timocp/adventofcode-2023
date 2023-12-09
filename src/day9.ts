408ec65d593e3309b4d4cc982110b7aa2b335672
Tim Peters <tim@catminion.net>
2023-12-09 16:00:38 +1030

    Day 9 part 1

---
+++ src/day9.ts
@@ -0,0 +1,27 @@
+import Solution from './solution'
+import { sum } from './util'
+
+export class Day9 extends Solution {
+  part1 (): number {
+    return sum(this.parseInput().map(data => this.nextValue(data)))
+  }
+
+  part2 (): number {
+    return 0
+  }
+
+  nextValue (data: number[]): number {
+    const diffs = data.slice(1).map((v, i) => v - data[i])
+    if (diffs.every(v => v === 0)) {
+      return data[data.length - 1] + diffs[0]
+    } else {
+      return data[data.length - 1] + this.nextValue(diffs)
+    }
+  }
+
+  parseInput (): number[][] {
+    return this.inputLines().map(line => line.split(' ').map(n => +n))
+  }
+}
+
+(new Day9()).solve()
