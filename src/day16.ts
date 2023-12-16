30cb4f79a704313b0975f60f74069978861dd0e0
Tim Peters <tim@catminion.net>
2023-12-16 20:37:34 +1030

    Speedup day 16

--- src/day16.ts
+++ src/day16.ts
@@ -35,30 +35,28 @@ interface Cell {
   energised: boolean
 }
 
+interface Beam {
+  x: number
+  y: number
+  dir: Dir
+}
+
 class Contraption {
-  // beams are a 3d array of boolean indicating at least one beam is present (3rd dimension is direction 0-3)
-  beams: boolean[][][]
   layout: Cell[][]
+  width: number
+  height: number
 
   constructor (input: string[]) {
-    this.beams = []
-    this.layout = input.map((line, y) => {
-      this.beams.push([])
+    this.layout = input.map(line => {
       return line.split('').map(c => {
-        this.beams[y].push([false, false, false, false])
         return { thing: c, energised: false }
       })
     })
-  }
-
-  emptyBeams (): boolean[][][] {
-    return this.beams.map(row => {
-      return row.map(_ => [false, false, false, false])
-    })
+    this.width = this.layout[0].length
+    this.height = this.layout.length
   }
 
   reset (): void {
-    this.beams = this.emptyBeams()
     this.layout.forEach(row => { row.forEach(cell => { cell.energised = false }) })
   }
 
@@ -72,123 +70,63 @@ class Contraption {
     return c
   }
 
-  // returns the energised cell count from given starting position
-  // TODO: can the state cache be shared?  I think it will need to be for part 2
-  // as each sim takes about a second and we'll need 440 runs
-  run (x: number, y: number, dir: Dir): number {
-    this.reset()
-    this.layout[y][x].energised = true
-    console.log(x, y, dir, this.layout[y][x])
-    // Direction of initial beam depends on mirror at 0,0
-    switch (this.layout[y][x].thing) {
-      case '.':
-        this.beams[y][x][dir] = true
-        break
-      case '\\':
-        this.beams[y][x][mirrorB[dir]] = true
-        break
-      case '/':
-        this.beams[y][x][mirrorA[dir]] = true
-        break
-      case '|':
-        if (dir === Dir.east || dir === Dir.west) {
-          this.beams[y][x][Dir.north] = true
-          this.beams[y][x][Dir.south] = true
-        } else {
-          this.beams[y][x][dir] = true
-        }
-        break
-      case '-':
-        if (dir === Dir.north || dir === Dir.south) {
-          this.beams[y][x][Dir.east] = true
-          this.beams[y][x][Dir.west] = true
-        } else {
-          this.beams[y][x][dir] = true
-        }
-        break
-      default:
-        throw new Error('Unhandled input')
-    }
+  run (startX: number, startY: number, startDir: Dir): number {
+    const stack: Beam[] = [{ x: startX, y: startY, dir: startDir }]
     const seen: Record<string, boolean> = {}
-    while (true) {
-      this.move()
-      const st = this.state()
-      if (seen[st] !== undefined) {
-        const a = this.countEnergised()
-        console.log(`Result for starting from ${x},${y} facing ${dir}`, a)
-        return a
-      }
-      seen[st] = true
-    }
-  }
+    this.reset()
 
-  // dump state to a unique string for loop detection
-  state (): string {
-    const st: string[] = []
-    for (let y = 0; y < this.beams.length; y++) {
-      for (let x = 0; x < this.beams[0].length; x++) {
-        st.push(String.fromCharCode(this.beams[y][x].reduce((acc, v) => acc << 1 | (v ? 1 : 0), 0) + 65))
-      }
-    }
-    return st.join('')
-  }
-
-  move (): void {
-    const newBeams = this.emptyBeams()
-
-    for (let y = 0; y < this.beams.length; y++) {
-      for (let x = 0; x < this.beams[0].length; x++) {
-        for (let dir: Dir = 0; dir < 4; dir++) {
-          if (this.beams[y][x][dir]) {
-            const [dx, dy] = delta[dir]
-            const nx = x + dx
-            const ny = y + dy
-            if (nx >= 0 && nx < this.beams[0].length && ny >= 0 && ny < this.beams.length) {
-              this.layout[ny][nx].energised = true
-              switch (this.layout[ny][nx].thing) {
-                case '.':
-                  newBeams[ny][nx][dir] = true
-                  break
-                case '/':
-                  newBeams[ny][nx][mirrorA[dir]] = true
-                  break
-                case '\\':
-                  newBeams[ny][nx][mirrorB[dir]] = true
-                  break
-                case '|':
-                  if (dir === Dir.east || dir === Dir.west) {
-                    newBeams[ny][nx][Dir.north] = true
-                    newBeams[ny][nx][Dir.south] = true
-                  } else {
-                    newBeams[ny][nx][dir] = true
-                  }
-                  break
-                case '-':
-                  if (dir === Dir.north || dir === Dir.south) {
-                    newBeams[ny][nx][Dir.east] = true
-                    newBeams[ny][nx][Dir.west] = true
-                  } else {
-                    newBeams[ny][nx][dir] = true
-                  }
-                  break
-                default:
-                  throw new Error('unhandled input')
-              }
+    while (stack.length > 0) {
+      const beam = stack.pop()
+      if (beam === undefined) throw new Error('unreachable')
+      while (true) {
+        const [dx, dy] = delta[beam.dir]
+        const nx = beam.x + dx
+        const ny = beam.y + dy
+        if (nx < 0 || nx >= this.width || ny < 0 || ny >= this.height) {
+          break
+        }
+        const key = `${nx},${ny},${beam.dir}`
+        if (seen[key] !== undefined) {
+          break
+        }
+        seen[key] = true
+        this.layout[ny][nx].energised = true
+        beam.x = nx
+        beam.y = ny
+        switch (this.layout[ny][nx].thing) {
+          case '.':
+            break
+          case '/':
+            beam.dir = mirrorA[beam.dir]
+            break
+          case '\\':
+            beam.dir = mirrorB[beam.dir]
+            break
+          case '|':
+            if (beam.dir === Dir.east || beam.dir === Dir.west) {
+              beam.dir = Dir.north
+              stack.push({ x: beam.x, y: beam.y, dir: Dir.south })
             }
-          }
+            break
+          case '-':
+            if (beam.dir === Dir.north || beam.dir === Dir.south) {
+              beam.dir = Dir.east
+              stack.push({ x: beam.x, y: beam.y, dir: Dir.west })
+            }
+            break
+          default:
+            throw new Error('unreachable')
         }
       }
     }
-
-    this.beams = newBeams
+    return this.countEnergised()
   }
 }
 
 export class Day16 extends Solution {
   part1 (): number {
     const contraption = this.parseInput()
-    contraption.run(0, 0, Dir.east)
-    return contraption.countEnergised()
+    return contraption.run(-1, 0, Dir.east)
   }
 
   part2 (): number {
@@ -196,15 +134,15 @@ export class Day16 extends Solution {
     let max = 0
     for (let x = 0; x < contraption.layout[0].length; x++) {
       const r = Math.max(
-        contraption.run(x, 0, Dir.south),
-        contraption.run(x, contraption.layout.length - 1, Dir.north)
+        contraption.run(x, -1, Dir.south),
+        contraption.run(x, contraption.layout.length, Dir.north)
       )
       if (r > max) max = r
     }
     for (let y = 0; y < contraption.layout.length; y++) {
       const r = Math.max(
-        contraption.run(0, y, Dir.east),
-        contraption.run(contraption.layout[0].length - 1, y, Dir.west)
+        contraption.run(-1, y, Dir.east),
+        contraption.run(contraption.layout[0].length, y, Dir.west)
       )
       if (r > max) max = r
     }
