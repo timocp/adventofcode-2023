750c44966192dfac60a2f75d0b24d4f89db6ecdd
Tim Peters <tim@catminion.net>
2023-12-16 12:28:36 +1030

    Day 12 rewrite arrangements

--- src/day12.ts
+++ src/day12.ts
@@ -1,51 +1,85 @@
 import Solution from './solution'
-import { kCombinations } from './combinations'
 import { sum } from './util'
 
-export const arrangements = (s: string): number => {
-  const [springs, countsStr] = s.split(' ')
-  const counts = countsStr.split(',').map(c => +c)
-  const unknown = []
-  const known = []
-  for (let i = 0; i < springs.length; i++) {
-    if (springs[i] === '?') unknown.push(i)
-    if (springs[i] === '#') known.push(i)
-  }
-  const missing = sum(counts) - known.length
-  if (missing === 0) return 1
-  let a = 0
-  kCombinations(unknown, missing).forEach(replace => {
-    const newSprings = springs.split('').map((c, i) => {
-      if (c === '?') {
-        if (replace.includes(i)) {
-          return '#'
-        } else {
-          return '.'
-        }
-      } else {
-        return c
-      }
-    }).join('')
-    if (valid(newSprings, counts)) a++
-  })
-  return a
+export const unfold = (line: string): string => {
+  const [s, c] = line.split(' ')
+  return `${s}?${s}?${s}?${s}?${s} ${c},${c},${c},${c},${c}`
 }
 
-const valid = (springs: string, counts: number[]): boolean => {
-  const newCounts = springs.replace(/^\.+/, '').replace(/\.+$/, '').split(/\.+/).map(s => s.length)
-  if (counts.length !== newCounts.length) {
-    return false
+interface Row {
+  s: string
+  c: number[]
+}
+
+const rowToString = (row: Row): string => `${row.s} ${row.c.join(',')}`
+
+const countArrangements = (row: Row): number => {
+  const key = rowToString(row)
+  const debug = false
+
+  if (row.s.length === 0) {
+    if (row.c.length === 0) {
+      if (debug) console.log(key, '-> 1 (row and counts are both empty)')
+      return 1
+    } else {
+      if (debug) console.log(key, '-> 0 (row is empty but counts remain)')
+      return 0
+    }
+  } else if (row.c.length === 0) {
+    if (row.s.includes('#')) {
+      if (debug) console.log(key, '-> 0 (counts empty but at least one broken remains)')
+      return 0
+    } else {
+      if (debug) console.log(key, '-> 1 (counts empty and no broken remains)')
+      return 1
+    }
   }
-  for (let i = 0; i < counts.length; i++) {
-    if (counts[i] !== newCounts[i]) return false
+  const s0 = row.s[0]
+  const c0 = row.c[0]
+  switch (s0) {
+    case '.':
+    {
+      const r = countArrangements({ s: row.s.substring(1), c: row.c })
+      if (debug) console.log(key, `-> ${r}`)
+      return r
+    }
+    case '?':
+    {
+      const a = countArrangements({ s: `.${row.s.substring(1)}`, c: row.c })
+      const b = countArrangements({ s: `#${row.s.substring(1)}`, c: row.c })
+      const r = a + b
+      if (debug) console.log(key, `-> ${a} + ${b} -> ${r}`)
+      return r
+    }
+    case '#':
+    {
+      if (row.s.length < c0) {
+        if (debug) console.log(key, '-> 0 (remaining blocks not big enough)')
+        return 0
+      }
+      if (row.s.substring(0, c0).includes('.')) {
+        if (debug) console.log(key, `-> 0 (leading block can't be of size ${c0})`)
+        return 0
+      }
+      if (row.s[c0] === '#') {
+        if (debug) console.log(key, '-> 0 (leading block is too big)')
+        return 0
+      }
+      const r = countArrangements({ s: row.s.substring(c0 + 1), c: row.c.filter((_, i) => i > 0) })
+      if (debug) console.log(key, `-> ${r}`)
+      return r
+    }
   }
-  return true
+  throw new Error(`Unhandled case: ${key}`)
+}
+
+export const arrangements = (s: string): number => {
+  const [springs, counts] = s.split(' ')
+  return countArrangements({ s: springs, c: counts.split(',').map(c => +c) })
 }
 
 export class Day12 extends Solution {
   part1 = (): number => sum(this.inputLines().map(line => arrangements(line)))
 
-  part2 (): number {
-    return 0
-  }
+  part2 = (): number => 0
 }
--- test/day12.spec.ts
+++ test/day12.spec.ts
@@ -1,6 +1,6 @@
 import { equal } from 'node:assert/strict'
 import { describe, it } from 'node:test'
-import { Day12, arrangements } from '../src/day12'
+import { Day12, arrangements, unfold } from '../src/day12'
 
 const testInput = `
 ???.### 1,1,3
@@ -11,14 +11,29 @@ const testInput = `
 ?###???????? 3,2,1
 `.trimStart()
 
+describe('unfold', () => {
+  it('unfold 1', () => { equal(unfold('.# 1'), '.#?.#?.#?.#?.# 1,1,1,1,1') })
+  it('unfold 2', () => { equal(unfold('???.### 1,1,3'), '???.###????.###????.###????.###????.### 1,1,3,1,1,3,1,1,3,1,1,3,1,1,3') })
+})
+
+describe('arrangements', () => {
+  it('arrangements 1', () => { equal(arrangements('???.### 1,1,3'), 1) })
+  it('arrangements 2', () => { equal(arrangements('.??..??...?##. 1,1,3'), 4) })
+  it('arrangements 3', () => { equal(arrangements('?#?#?#?#?#?#?#? 1,3,1,6'), 1) })
+  it('arrangements 4', () => { equal(arrangements('????.#...#... 4,1,1'), 1) })
+  it('arrangements 5', () => { equal(arrangements('????.######..#####. 1,6,5'), 4) })
+  it('arrangements 6', () => { equal(arrangements('?###???????? 3,2,1'), 10) })
+  it('arrangements 7', () => { equal(arrangements('?##??.?#?? 2,1'), 1) })
+  it('arrangements 8', () => { equal(arrangements('##?#?#?? 2,6'), 0) })
+  it('arrangements folded 1', () => { equal(arrangements(unfold('???.### 1,1,3')), 1) })
+  it('arrangements folded 2', () => { equal(arrangements(unfold('.??..??...?##. 1,1,3')), 16384) })
+  it('arrangements folded 3', () => { equal(arrangements(unfold('?#?#?#?#?#?#?#? 1,3,1,6')), 1) })
+  it('arrangements folded 4', () => { equal(arrangements(unfold('????.#...#... 4,1,1')), 16) })
+  it('arrangements folded 5', () => { equal(arrangements(unfold('????.######..#####. 1,6,5')), 2500) })
+  it('arrangements folded 6', () => { equal(arrangements(unfold('?###???????? 3,2,1')), 506250) })
+})
+
 describe('Day12', () => {
   const d = new Day12(testInput)
-  it('arrangements example 1', () => { equal(arrangements('???.### 1,1,3'), 1) })
-  it('arrangements example 2', () => { equal(arrangements('.??..??...?##. 1,1,3'), 4) })
-  it('arrangements example 3', () => { equal(arrangements('?#?#?#?#?#?#?#? 1,3,1,6'), 1) })
-  it('arrangements example 4', () => { equal(arrangements('????.#...#... 4,1,1'), 1) })
-  it('arrangements example 5', () => { equal(arrangements('????.######..#####. 1,6,5'), 4) })
-  it('arrangements example 6', () => { equal(arrangements('?###???????? 3,2,1'), 10) })
-  it('arrangements example 7', () => { equal(arrangements('?##??.?#?? 2,1'), 1) })
-  it('part1 example1', () => { equal(d.part1(), 21) })
+  it('part1', () => { equal(d.part1(), 21) })
 })
