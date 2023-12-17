9ee48550f0ebd7b32dabbfba4b8dfb3ca378e738
Tim Peters <tim@catminion.net>
2023-12-17 22:29:01 +1030

    Speed up day 17 by using a binary heap PQ

--- src/priorityQueue.ts
+++ src/priorityQueue.ts
@@ -1,41 +1,60 @@
-// TODO: reverse the list, so we can pop to get the head
-// TODO: Or, reimplement as a heap
-
 type PriorityQueueItem<T> = [number, T]
 
 export class PriorityQueue<T> {
-  private readonly items: Array<PriorityQueueItem<T>>
+  private readonly heap: Array<PriorityQueueItem<T>>
 
   constructor () {
-    this.items = []
+    this.heap = []
   }
 
   push (item: T, priority: number): void {
-    if (this.items.length === 0) {
-      this.items.push([priority, item])
-    } else {
-      let added = false
-      for (let i = 0; i < this.items.length; i++) {
-        if (priority < this.items[i][0]) {
-          this.items.splice(i, 0, [priority, item])
-          added = true
-          break
-        }
-      }
-      if (!added) {
-        this.items.push([priority, item])
-      }
-    }
+    this.heap.push([priority, item])
+    this.bubbleUp()
   }
 
   pop (): T {
-    const item = this.items.shift()
-    if (item === undefined) throw new Error('Attempted to pop from empty queue')
-    return item[1]
+    if (this.heap.length === 0) throw new Error('Attempted to pop from an empty priority queue')
+    const min = this.heap[0]
+    if (this.heap.length > 1) this.heap[0] = this.heap[this.heap.length - 1]
+    this.heap.pop()
+    this.bubbleDown()
+    return min[1]
   }
 
-  size = (): number => this.items.length
+  private bubbleUp (): void {
+    let index = this.heap.length - 1
+    while (index > 0) {
+      const parentIndex = Math.floor((index - 1) / 2)
+      if (this.heap[parentIndex][0] < this.heap[index][0]) break
+      this.swap(parentIndex, index)
+      index = parentIndex
+    }
+  }
 
-  // debugging only
-  debug = (): Array<PriorityQueueItem<T>> => this.items
+  private bubbleDown (): void {
+    let index = 0
+    while (true) {
+      const leftIndex = 2 * index + 1
+      const rightIndex = 2 * index + 2
+      if (leftIndex >= this.heap.length) break
+      let smallerChildIndex = leftIndex
+      if (rightIndex < this.heap.length && this.heap[rightIndex][0] < this.heap[leftIndex][0]) {
+        smallerChildIndex = rightIndex
+      }
+      if (this.heap[index][0] < this.heap[smallerChildIndex][0]) {
+        break
+      } else {
+        this.swap(index, smallerChildIndex)
+      }
+      index = smallerChildIndex
+    }
+  }
+
+  private swap (source: number, target: number): void {
+    const tmp = this.heap[source]
+    this.heap[source] = this.heap[target]
+    this.heap[target] = tmp
+  }
+
+  size = (): number => this.heap.length
 }
